2e7a6a432df588a7423ed75d2d7227a3d2489848
patchayanin <punch2103@gmail.com>
2022-01-16 23:52:02 +0700

    add contact us page

--- BasicFirstApp/BasicApp/Views/ContactUsPage.swift
+++ BasicFirstApp/BasicApp/Views/ContactUsPage.swift
@@ -10,12 +10,47 @@ import SwiftUI
 struct ContactUsPage: View {
     var body: some View {
         VStack {
+            
+            Spacer()
             Image("LangsBerryContactUsLogo")
                           .resizable()
-                          .frame(width: 210, height: 134)
+                          .frame(width: 280, height: 160)
                           .padding(.bottom)
             
             Text("สอบถามข้อมูล / สมัครเรียนได้ที่")
+                .padding(.bottom)
+            HStack{
+                Image("TwitterLogo")
+                    .resizable()
+                    .frame(width: 56, height: 56)
+                    .padding()
+                
+                Image("LINELogo")
+                    .resizable()
+                    .frame(width: 56, height: 56)
+                    .padding()
+                
+                Image("InstagramLogo")
+                    .resizable()
+                    .frame(width: 56, height: 56)
+                    .padding()
+                
+                Image("FacebookLogo")
+                    .resizable()
+                    .frame(width: 56, height: 56)
+                    .padding()
+                
+            }.padding(.bottom)
+            
+            
+            Text("อ่ายรายละเอียดเพิ่มเติมได้ที่")
+
+            Text("www.langsberry.com")
+                .foregroundColor(Color("LangsBerryColor"))
+            
+            Spacer()
+            Text("2022 SkyLine. All Rights Reserved")
+            
         }
                   
     }
@@ -26,3 +61,4 @@ struct ContactUsPage_Previews: PreviewProvider {
         ContactUsPage()
     }
 }
+
